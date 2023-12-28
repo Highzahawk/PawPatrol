6008db44e255af64a63c02965059322803f5d6ea
s-rsunku <rsunku@ucsd.edu>
2023-12-27 19:31:40 -0800

    Finished authorization pages/functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import AuthLayout from './_auth/AuthLayout';
 import { Home } from './_root/pages';
 import './globals.css';
 import RootLayout from './_root/RootLayout';
+import { Toaster } from "@/components/ui/toaster"
+
 
 
 const App = () => {
@@ -23,6 +25,8 @@ const App = () => {
                 <Route index element={<Home />}/>
             </Route>
         </Routes>
+
+        <Toaster />
     </main>
   )
 }
